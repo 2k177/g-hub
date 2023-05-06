726076c02089fcc1d5546cdb8679c482e7278ca1
Danusha U <danuu@qti.qualcomm.com>
2023-05-06 18:08:57 +0530

    Add user login url, avatar and username in results section

--- src/App.jsx
+++ src/App.jsx
@@ -20,23 +20,54 @@ function App() {
   const onSearchSubmit = async (ev) => {
     ev.preventDefault();
     console.log("on submit");
-    const result = await githubResult(query);
-    setResults(result);
+    const results = await githubResult(query);
+    setResults(results);
     console.log(results);
   };
 
   return (
     <div>
       <h2>Github API</h2>
-      <form onChange={onSearchChange} onSubmit={onSearchSubmit}>
-        <label>
-          Enter user name:
-          <input type="text" />
-        </label>
-      </form>
-      {/* <form onChange={onSearchChange} onSubmit={onSearchSubmit} value={query} /> */}
+      <Form onChange={onSearchChange} onSubmit={onSearchSubmit} value={query} />
+      <h3>Results</h3>
+      <div>
+        {results.map((user) => (
+          <User
+            key={user.login}
+            avatar={user.avatar_url}
+            url={user.url}
+            username={user.login}
+          />
+        ))}
+      </div>
     </div>
   );
 }
 
 export default App;
+
+function User({ avatar, url, username }) {
+  return (
+    <div className="User">
+      <img src={avatar} alt="Profile" width="50" height="50" />
+      <a href={url} target="_blank" rel="noopener noreferrer">
+        {username}
+      </a>
+    </div>
+  );
+}
+
+function Form({ onSubmit, onChange, value }) {
+  return (
+    <form className="search-form" onSubmit={onSubmit}>
+      <input
+        id="search"
+        type="text"
+        placeholder="Enter username or email"
+        onChange={onChange}
+        value={value}
+      />
+      <button type="submit">Search</button>
+    </form>
+  );
+}
